ee95d819380febde93522002fcadd0a1d3c9dc5e
Vytautas <Vytautas@iMac-de-Pablo.local>
2018-02-05 17:03:06 +0100

    updated

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+// Initialize tooltip component
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+})
+
+// Initialize popover component
+$(function () {
+  $('[data-toggle="popover"]').popover();
+})
